cf097bcceaf0283e4de6a9d578ba87447552e220
leokajdic <kajdic56@gmail.com>
2023-06-21 09:55:11 +0200

    Creazione lista cars

--- ServerEsame/src/main/java/org/example/App.java
+++ ServerEsame/src/main/java/org/example/App.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static List<Car> cars = new ArrayList<Car>();
 
+    static void buildList() {
+        cars.add(new Car(123, "bmw", 3594.9));
+        cars.add(new Car(123, "bmw", 38346.9));
+        cars.add(new Car(135, "ferrari", 130000.4));
+        System.out.println(cars);
+    }
     public static void main( String[] args )
     {
-
+        buildList();
     }
 }
